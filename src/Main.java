5d7b61019f7c921d8a75d0c2aa52b0ebcd24d0e2
mjp <sayhello_mjp@163.com>
2017-09-05 13:47:43 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
+    {
+        System.out.println("构造代码块");
+    }
+
+    public Main(){
+        System.out.println("构造函数1");
+    }
+
+    public Main(String test){
+        System.out.println("构造函数2");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main m = new Main();
     }
 }
